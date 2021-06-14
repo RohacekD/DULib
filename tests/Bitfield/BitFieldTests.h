d2f9af1716ca50f75de9964e965ac7531c5587aa
RohacekD <RohacekD@gmail.com>
2021-06-14 10:13:30 +0200

    Add tests
    BitField(Enum)
    SetFlag(enum)
    SetFlags(BitField)
    ToggleFlag(enum)

--- tests/Bitfield/BitFieldTests.h
+++ tests/Bitfield/BitFieldTests.h
@@ -10,23 +10,64 @@
 
 enum class E_TestEnum : std::uint8_t
 {
-	flag1 = 1,
-	flag2 = 2,
-	flag3 = 4,
-	flag4 = 8,
+  flag1 = 1,
+  flag2 = 2,
+  flag3 = 4,
+  flag4 = 8,
 };
 namespace DULib {
 TEST_CASE("Bit fields", "[BitField]") {
-	SECTION("Initialization") {
-		BitField<E_TestEnum> e;
+	// start with default ctor
+	BitField<E_TestEnum> e;
 
-		// default ctor should init bit field to all 0
+	// default ctor should init bit field to all 0
+	REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag1));
+	REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag2));
+	REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag3));
+	REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag4));
+
+	REQUIRE(e.GetFlags() == 0);
+
+
+	SECTION("BitField(Enum)") {
+		BitField<E_TestEnum> e2(E_TestEnum::flag2);
+		REQUIRE_FALSE(e2.CheckFlag(E_TestEnum::flag1));
+		REQUIRE(e2.CheckFlag(E_TestEnum::flag2)); //<!!!!!!!!
+		REQUIRE_FALSE(e2.CheckFlag(E_TestEnum::flag3));
+		REQUIRE_FALSE(e2.CheckFlag(E_TestEnum::flag4));
+	}
+
+	SECTION("SetFlag(enum)") {
+		e.SetFlag(E_TestEnum::flag2);
 		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag1));
-		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag2));
+		REQUIRE(e.CheckFlag(E_TestEnum::flag2)); //<!!!!!!!!
 		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag3));
 		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag4));
+	}
 
-		REQUIRE(e.GetFlags() == 0);
+	SECTION("SetFlags(BitField)") {
+		BitField<E_TestEnum> e2(E_TestEnum::flag2);
+		e2.SetFlag(E_TestEnum::flag1);
+		REQUIRE(e2.CheckFlag(E_TestEnum::flag1));
+		REQUIRE(e2.CheckFlag(E_TestEnum::flag2));
+		e.SetFlags(e2);
+		REQUIRE(e.CheckFlag(E_TestEnum::flag1));
+		REQUIRE(e.CheckFlag(E_TestEnum::flag2));
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag3));
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag4));
+	}
+
+	SECTION("ToggleFlag(enum)") {
+		e.ToggleFlag(E_TestEnum::flag2);
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag1));
+		REQUIRE(e.CheckFlag(E_TestEnum::flag2)); //<!!!!!!!!
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag3));
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag4));
+		e.ToggleFlag(E_TestEnum::flag2);
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag1));
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag2)); //<!!!!!!!!
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag3));
+		REQUIRE_FALSE(e.CheckFlag(E_TestEnum::flag4));
 	}
 }
 }
